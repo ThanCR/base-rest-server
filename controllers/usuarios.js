5b2c3943b6b14cb2b914575dee7d2c74803cff08
Jonnathan <jonzxz12@gmail.com>
2022-11-15 13:43:34 -0600

    Usuarios CRUD listo - 11-15-2022

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,45 +1,71 @@
-const { response, request } = require('express')
+const { response, request } = require('express');
+const Usuario = require('../models/usuario')
+const bcryptjs = require('bcryptjs');
 
 
 
-const usuariosGet = (req = request, res = response) =>{
-    
-    const {q, apikey} = req.query
+const usuariosGet = async (req = request, res = response) => {
 
+    const query = {estado:true}
+    const { limite = 5, desde = 0 } = req.query//query son todos los parametros adicionales que envio por el URL despues del simbolo ? y separados por &
+
+    //Es la forma de enviar la respuesta con ambos resultados a la vez, debido a que son estructuras bloqueantes
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite))//Establece un limite de registros
+    ])
     res.json({
-        msg: 'get API - controlador',
-        q,
-        apikey
+        total,
+        usuarios
     })
 }
-const usuariosPut = (req, res = response) =>{
-    
-    const id = req.params.id
-    
+const usuariosPut = async (req, res = response) => {
 
-    res.json({
-        msg: 'put API - controlador',
-        id
-    })
+    const { id } = req.params;
+    const { _id, password, google, correo, ...resto } = req.body
+
+    if (password) {
+        //Encriptar la contraseña
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync(password, salt);
+    }
+
+    const usuario = await Usuario.findByIdAndUpdate(id, resto)
+
+    res.json(usuario)
 }
-const usuariosPost = (req, res = response) =>{
-    
-    const {nombre, edad} = req.body;
+const usuariosPost = async (req, res = response) => {
+
+
+
+    const { nombre, correo, password, rol } = req.body;
+
+    const usuario = new Usuario({ nombre, correo, password, rol });
+
+    //Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+
+    //Guardar  en BD
+    await usuario.save()
 
     res.json({
         msg: 'post API - controlador',
-        nombre,
-        edad
+        usuario
     })
 }
-const usuariosDelete = (req, res = response) =>{
-    
-    res.json({
-        msg: 'delete API - controlador'
-    })
+const usuariosDelete = async(req, res = response) => {
+
+    const { id} = req.params
+
+     const usuario = await Usuario.findByIdAndUpdate(id, {estado:false});
+
+    res.json(usuario)
 }
-const usuariosPatch = (req, res = response) =>{
-    
+const usuariosPatch = (req, res = response) => {
+
     res.json({
         msg: 'patch API - controlador'
     })
---
+++ models/usuario.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose')
+
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo: {
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatorio']
+    },
+    img: {
+        type: String
+    },
+    rol: {
+        type: String,
+        required: true
+    },
+    estado: {
+        type: Boolean,
+        default: false
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+})
+
+//En esta linea, tiene que ser una funcion normal y no de flecha, para que no tenga conflictos con el this.variable
+//Logica para eliminar ciertos campos a la hora de dar las respuestas o manipular el objeto en codigo, como tal se usa para evitar enviar contraseñas 
+//o informacion sensible de la base de datos
+UsuarioSchema.methods.toJSON = function(){
+    const {__v, password, ...usuario} = this.toObject();
+    return usuario;
+}
+
+module.exports = model('Usuario', UsuarioSchema)
